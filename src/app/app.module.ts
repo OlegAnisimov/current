f6084a6922b0520b746734748f6181dd77a85427
IvanAleeksevyandexru <105803627+IvanAleeksevyandexru@users.noreply.github.com>
2022-05-31 12:23:34 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import { BasicMatTableComponent } from './basic-mat-table/basic-mat-table.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { StreamsComponent } from './streams/streams.component';
+import { AuditComponent } from './audit/audit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicMatTableComponent,
+    StreamsComponent,
+    AuditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
